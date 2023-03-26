67ab04384632ac65b50f3b0162c438c88d414634
Waros <wyayla@gmail.com>
2023-03-26 14:32:46 +0200

    Initial project

---
+++ app/src/main/java/avans/groep15/themoviedb/presentation/MainActivity.java
@@ -0,0 +1,16 @@
+package avans.groep15.themoviedb.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import avans.groep15.themoviedb.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
